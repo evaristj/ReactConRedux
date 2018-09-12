f69c856cf226b17562f12c30de5e48538795239f
Evarist Jaume <evaristjaumet@gmail.com>
2018-09-12 17:34:41 +0200

    create actions, reducers, store, etc

---
+++ src/actions/productActions.js
@@ -0,0 +1,102 @@
+import {
+  FETCH_PRODUCT_INIT,
+	FETCH_PRODUCTS_SUCCESS,
+	FETCH_PRODUCTS_FAILURE,
+	FETCH_PRODUCT_SUCCESS,
+	FETCH_PRODUCT_FAILURE,
+// When call to BBDD
+  SAVE_PRODUCT_INIT,
+	SAVE_PRODUCT_SUCCESS,
+	SAVE_PRODUCT_FAILURE
+} from 'types';
+import API from '../api';
+
+// Actions Creators
+export function fetchProductsSuccess (products){
+	return {
+		type: FETCH_PRODUCTS_SUCCESS,
+		payload: products
+	};
+}
+
+export function fetchProductsFailure (error){
+	return {
+		type: FETCH_PRODUCTS_FAILURE,
+		payload: error
+	};
+}
+
+export function fetchProductSuccess (product){
+	return {
+		type: FETCH_PRODUCT_SUCCESS,
+		payload: product
+	};
+}
+
+export function fetchProductFailure (error){
+	return {
+		type: FETCH_PRODUCT_FAILURE,
+		payload: error
+	};
+}
+
+export function saveProductSuccess (){
+	return {
+		type: SAVE_PRODUCT_SUCCESS,
+	};
+}
+
+export function saveProductFailure (error){
+	return {
+		type: SAVE_PRODUCT_FAILURE,
+		payload: error
+	};
+}
+	 
+// Actions Creators (Async)
+export function fetchProducts () {
+  return async (dispatch => {
+    dispatch(() => {
+      return {
+        type: FETCH_PRODUCT_INIT
+      };
+    });
+
+    try {
+      const data = await API.products.getAll();
+      return dispatch(fetchProductsSuccess(data.products));
+    } catch (error) {
+      return dispatch(fetchProductFailure(error));
+    }
+  });
+}
+export function fetchProduct (productId) {
+  return async (dispatch => {
+    dispatch(() => {
+      return {
+        type: FETCH_PRODUCT_INIT
+      };
+    });
+    try {
+      const data = await API.products.getSingle(productId);
+      return dispatch(fetchProductSuccess(data.product));
+    } catch (error) {
+      return dispatch(fetchProductFailure(error));
+    }
+  });
+}
+export function saveProduct (product) {
+return async (dispatch => {
+  dispatch(() => {
+    return {
+      type: SAVE_PRODUCT_INIT
+    }
+  });
+  try {
+    await API.products.save(product);
+    return dispatch(saveProductSuccess());
+  } catch (error) {
+    return dispatch(saveProductFailure(error));
+  }
+});
+}
---
+++ src/products/ProductListContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// redux container
+class ProductListContainer extends Component {
+    render () {
+        return (
+            <div>
+                <h1> Hola Redux!!!</h1>
+            </div>
+        );
+    }
+}
+
+export default ProductListContainer;
